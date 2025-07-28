317110d25c4224e36bb35248bfab60d80332d7e0
nikojtgessner@gmail.com <nikojtgessner@gmail.com>
2025-07-28 19:24:42 +0000

    Edited settings and Pages.Pictures

--- client_code/Pages/Pictures/__init__.py
+++ client_code/Pages/Pictures/__init__.py
@@ -16,7 +16,6 @@ class Pictures(PicturesTemplate):
     self.layout.pictures_link.selected = True
     self.additional_pics = []
     self.floorplan_pics = []
-    self.files_list.add_event_handler('x-delete-file-item', self.handle_delete_item)
     self.floorplan_files.add_event_handler('x-delete-floorplan-item', self.handle_delete_item_floorplan)
     
 
@@ -60,17 +59,6 @@ class Pictures(PicturesTemplate):
       self.floorplan_pics.append(fl)
     self.floorplan_files.items = self.floorplan_pics
 
-  def additional_images_change(self, file, **event_args):
-    """This method is called when a new file is loaded into this FileLoader"""
-    for fl in self.additional_images.files:
-      self.additional_pics.append(fl)
-    self.files_list.items = self.additional_pics
-
-  def handle_delete_item(self, item_to_delete, **event_args):
-    """Handle delete event from repeating panel items"""
-    if item_to_delete in self.additional_pics:
-      self.additional_pics.remove(item_to_delete)
-      self.files_list.items = self.additional_pics
  
   def handle_delete_item_floorplan(self, item_to_delete, **event_args):
     """Handle delete event from repeating panel items"""
@@ -91,44 +79,7 @@ class Pictures(PicturesTemplate):
     else:
       return image
 
-  # def read_current_files(self, colunm):
-  #   """Extract images from the ZIP file"""
-
-  #   current_user = anvil.users.get_user()
-  #   if current_user is None:
-  #     return {"success": False, "images": []}
-
-  #   try:
-  #     if current_user is None or current_user[colunm] is None:
-  #       return {"success": True, "images": []}
-
-  #     zip_media = current_user[colunm]
-  #     zip_data = zip_media.get_bytes()
-
-  #     images = []
-  #     with zipfile.ZipFile(io.BytesIO(zip_data), 'r') as zip_file:
-  #       for filename in zip_file.namelist():
-  #         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-  #           image_data = zip_file.read(filename)
-
-  #         # Determine content type
-  #           if filename.lower().endswith('.png'):
-  #             content_type = 'image/png'
-  #           else:
-  #             content_type = 'image/jpeg'
-  
-  #           # Create Media object
-  #           image_media = BlobMedia(
-  #             content_type=content_type,
-  #             content=image_data,
-  #             name=filename
-  #           )
-  #           images.append(image_media)
-
-  #     return {"success": True, "images": images}
-
-  #   except Exception as e:
-  #     return {"success": False, "message": f"Error: {str(e)}", "images": []}
+ 
 
   
 
