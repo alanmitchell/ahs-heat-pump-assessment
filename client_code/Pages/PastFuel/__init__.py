45a2ec4d66dadbdb921c92f993f66d97a4c2f5f0
tabb99@gmail.com <tabb99@gmail.com>
2025-09-11 23:19:18 +0000

    Edited settings, google_util and Pages.PastFuel

--- client_code/Pages/PastFuel/__init__.py
+++ client_code/Pages/PastFuel/__init__.py
@@ -11,3 +11,7 @@ class PastFuel(PastFuelTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.layout.past_use_link.selected = True
+
+  def but_open_historical_ss_click(self, **event_args):
+    """This method is called when the component is clicked."""
+    new_file_id = anvil.server.call('make_client_historical_use_ss', )
--- server_code/google_util.py
+++ server_code/google_util.py
@@ -1,9 +1,9 @@
 """Module to interact with Google files and documents.
 """
 import time, json
-from typing import Dict
 
-import anvil.users
+from anvil.users import get_user
+from anvil import app_tables
 import anvil.secrets
 
 from google.oauth2 import service_account
@@ -32,10 +32,9 @@ def _retry(func, *args, **kwargs):
         continue
       raise
 
-@anvil.server.callable
 def copy_template_into_folder(template_file_id: str,     # Google ID of template file
                               target_folder_id: str,     # Google ID of target folder
-                              new_name: str) -> Dict[str, str]:     # New name of copied file
+                              new_name: str) -> str:     # New name of copied file
   """Copies a template file into a target folder and gives the new file a name.
   Returns the Google ID of the newly created file.
   """
@@ -47,5 +46,19 @@ def copy_template_into_folder(template_file_id: str,     # Google ID of template
     body={'name': new_name, 'parents': [target_folder_id]}
   )
   new_file = _retry(copy_req.execute)
-
   return new_file['id']
+
+@anvil.server.callable
+def make_client_historical_use_ss(ss_name: str) -> str:
+  """Creates a new Historical Use Google Sheet for a client and returns the Google
+  file ID of that Sheet. Uses a template file to create the sheet.
+  """
+  if get_user():
+    # only logged-in users can access.
+    template_id = app_tables.settings.search(key="historical-use-file-id")[0]["value"]
+    folder_id = app_tables.settings.search(key="client-document-folder-id")[0]["value"]
+    new_file_id = copy_template_into_folder(template_id, folder_id, ss_name)
+    return new_file_id
+
+  else:
+    return None
