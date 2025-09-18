b0f94b973ea2d4bb6aa4c05a227f3e0809a34528
tabb99@gmail.com <tabb99@gmail.com>
2025-09-18 20:45:44 +0000

    Edited calculator_api and Pages.ModelInputs

--- client_code/Pages/ModelInputs/__init__.py
+++ client_code/Pages/ModelInputs/__init__.py
@@ -5,6 +5,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.http
 from ...Utility import chg_none, active_client_name
 
 class ModelInputs(ModelInputsTemplate):
@@ -13,6 +14,18 @@ class ModelInputs(ModelInputsTemplate):
     self.init_components(**properties)
     self.layout.model_inputs_link.selected = True
     self.set_event_handler('show', self.form_show)
+
+    # get base URL for Heatpump Calculator API
+    self.calc_api_url = anvil.server.call('calculator_api_base_url')
+
+    # load the modeling city dropdown.
+    print(self.calc_api_url+ 'lib/cities')
+    resp = anvil.http.request(
+      self.calc_api_url + 'lib/cities',
+      method="GET",
+      json=True
+    )
+    print(resp)
 
   def form_show(self, **event_args):
     self.layout.rich_text_client_name.content = f'**Client:** {active_client_name()}'
