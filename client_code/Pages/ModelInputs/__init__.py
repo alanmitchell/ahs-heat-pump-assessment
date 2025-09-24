e5e55b77b26ce2756f08767e8a5623702b14ecf4
tabb99@gmail.com <tabb99@gmail.com>
2025-09-24 02:08:45 +0000

    Edited Pages.ModelInputs

--- client_code/Pages/ModelInputs/__init__.py
+++ client_code/Pages/ModelInputs/__init__.py
@@ -25,15 +25,36 @@ class ModelInputs(ModelInputsTemplate):
       method="GET",
       json=True
     )
-    # this will contain tuples of city, city_ID
+    # this will contain tuples of city, city_ID , sorted by city
     city_list = [rec['label'] for rec in resp]
-    city_list.sort()
     self.autocomplete_model_city.suggestions = city_list
     self.autocomplete_model_city.suggest_if_empty = True
     self.autocomplete_model_city.filter_mode = 'startswith'
+
+    # also make a dictionary to map city name to it's ID
+    self.city_map = {item['label']: item['id'] for item in resp}
 
     # load the Assessor dropdown
     self.dropdown_menu_assessor.items = [(u['full_name'], u['id']) for u in anvil.server.call("get_users_public_fields")]
 
   def form_show(self, **event_args):
     self.layout.rich_text_client_name.content = f'**Client:** {active_client_name()}'
+
+  def autocomplete_model_city_change(self, **event_args):
+    # if the change results in a valid city, populate the fuel price inputs
+    if self.autocomplete_model_city.text in self.city_map.keys():
+    # lookup info for the city.
+      city_id = self.city_map[self.autocomplete_model_city.text]
+      city = anvil.http.request(
+        self.calc_api_url + f'lib/cities/{city_id}',
+        method="GET",
+        json=True
+      )
+      self.text_box_oil_price.text = city['Oil1Price'] or city['Oil2Price']
+      self.text_box_propane_price.text = city['PropanePrice']
+      self.text_box_ng_price.text = '%.2f' % city['GasPrice'] if city['GasPrice'] else None
+      self.text_box_birch_price.text = city['BirchPrice']
+      self.text_box_spruce_price.text = city['SprucePrice']
+
+      
+    
