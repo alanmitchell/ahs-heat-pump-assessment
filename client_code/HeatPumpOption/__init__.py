9d0e67482980bbab92fd79f48fd1c98b6779c635
tabb99@gmail.com <tabb99@gmail.com>
2025-10-13 22:02:38 +0000

    Edited HeatPumpOption

--- client_code/HeatPumpOption/__init__.py
+++ client_code/HeatPumpOption/__init__.py
@@ -6,7 +6,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ..Utility import chg_none
 
 class HeatPumpOption(HeatPumpOptionTemplate):
   def __init__(self, **properties):
@@ -14,3 +14,18 @@ class HeatPumpOption(HeatPumpOptionTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def recalc_cost_totals(self, **event_args):
+    """This method is called when the text in this component is edited."""
+    try:
+      print(self.text_box_cost_hp_install.text)
+      print(self.text_box_cost_electrical.text)
+      print(chg_none(self.text_box_cost_electrical.text, 0))
+      print(float(chg_none(self.text_box_cost_hp_install.text, 0)))
+      sys_cost = chg_none(self.text_box_cost_hp_install.text, 0) + \
+          float(chg_none(self.text_box_cost_electrical.text, 0)) + \
+          float(chg_none(self.text_box_cost_permit.text, 0))
+      self.text_system_cost.text = f'${sys_cost:,.0f}'
+    except Exception as e:
+      print(e)
+      self.text_system_cost.text = ''
