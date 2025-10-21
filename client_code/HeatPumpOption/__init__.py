b04c53a78fb67c1eac286db9934de73846552fee
tabb99@gmail.com <tabb99@gmail.com>
2025-10-21 18:12:58 +0000

    Edited HeatPumpOption

--- client_code/HeatPumpOption/__init__.py
+++ client_code/HeatPumpOption/__init__.py
@@ -14,6 +14,7 @@ class HeatPumpOption(HeatPumpOptionTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.dropdown_menu_dhw_source.items = Library.DHW_AFTER_HP
+    self.dropdown_menu_dhw_after_fuel.items = Library.FUELS_ALL
 
   def recalc_cost_totals(self, **event_args):
     """This method is called when the text in this component is edited."""
@@ -25,3 +26,17 @@ class HeatPumpOption(HeatPumpOptionTemplate):
     total_cost = sys_cost + text_to_float(self.text_box_cost_non_hp) - \
       text_to_float(self.text_box_hp_incentives) - text_to_float(self.text_box_hp_tax_credit)
     self.text_total_cost.text = f'${total_cost:,.0f}'
+
+  def dropdown_menu_dhw_source_change(self, **event_args):
+    """Determine whether more info is needed."""
+    dhw_type = self.dropdown_menu_dhw_source.selected_value
+    if dhw_type in (3, 4, 5):
+      self.grid_panel_new_dhw.visible = True
+      if dhw_type == 5:
+        self.dropdown_menu_dhw_after_fuel.selected_value = 1
+        self.dropdown_menu_dhw_after_fuel.enabled = False
+      else:
+        self.dropdown_menu_dhw_after_fuel.enabled = True
+
+    else:
+      self.grid_panel_new_dhw.visible = False
