8a5da4ec26d49afdcea115f71a5dff5f72a6c1e3
tabb99@gmail.com <tabb99@gmail.com>
2025-12-16 23:10:15 +0000

    Edited Calculator

--- client_code/Calculator.py
+++ client_code/Calculator.py
@@ -15,55 +15,48 @@ def analyze_options(ui_inputs):
 def make_api_analyze_inputs(ui_inputs):
   # shortcut variable
   inp = ui_inputs
+
+  # Energy Prices sub-dictionary
+  energy_prices = {
+    
+  }
+
+  # Tuple containing Primary and Secondary conventional heating systems
+  conventional_systems = (
+    {
+      
+    },
+    {
+      
+    }
+  )
   
-  api_inp = {
+  building = {
 '''
-  'city_id': inp['model_city]
-
-  energy_prices: EnergyPrices    # Energy price information for the building
-
-  # Description of non-heat-pump heating systems: primary and secondary (optional)
-  conventional_heat: Tuple[ConventionalHeatingSystem, ConventionalHeatingSystem | None]
-
-  heat_pump: HeatPump | None = (
-    None  # Description of Heat Pump. If None, then no heat pump.
-  )
-  
-  garage_stall_count: int  # 0: No garage, 1: 1-car garage.  Max is 4.
-  bldg_floor_area: (
-    float  # Floor area in square feet of home living area, not counting garage.
-  )
-  occupant_count: float = 3.0    # Number of occupants for estimating non-space energy use
-  indoor_heat_setpoint: float = 70.0  # Indoor heating setpoint, deg F
-  ua_per_ft2: float    # UA per square foot for main home
-  
-  dhw_fuel_id: Fuel_id | None = None       # ID of domestic hot water fuel
-  dhw_ef: float = 0.62                     # Energy Factor of DHW System
-  clothes_drying_fuel_id: Fuel_id | None = None   # ID of clothes drying fuel
-  cooking_fuel_id: Fuel_id | None = None   # ID of cooking fuel
-  
-  # annual average kWh/day for lights and miscellaneous appliances end uses. 
-  # Does not include space htg, dhw, cooking, and clothes drying, EV charging, or solar.
-  misc_elec_kwh_per_day: float = 13.0
-  
-  # +/- deviation in use/day from average for December and June. 
-  # Expressed as a fraction of the average. If positive
-  # December is higher than average and June is lower. If negative,
-  # December is lower than average and June higher (perhaps snowbird usage pattern)
-  misc_elec_seasonality: float = 0.15  
-  
-  # Infomration about Home EV charging electricity use.
-  ev_charging_miles_per_day: float = 0.0           # average miles / day of home EV charging
-  ev_miles_per_kwh: float = 3.0            # Average miles driven per kWh of charge
-  ev_seasonality: float = 0.0              # Variation of Dec EV kWh/day compared to average, fraction
-  
-  solar_kw: float = 0.0                    # kW of home PV solar
-  solar_kwh_per_kw: float = 700.0          # Annual kWh produced per solar kW installed
-  
+  'city_id': inp['model_city'],
+  'energy_prices': energy_prices,
+  'conventional_heat': conventional_systems,
+  'heat_pump': None,
+  'garage_stall_count': inp['garage_count'],
+  'bldg_floor_area': inp['floor_area'],
+  'occupant_count': inp['occupant_count'],
+  'indoor_heat_setpoint': 70.0,
+  'ua_per_ft2': 0.19,    # will be changed in model fitting
+  'dhw_fuel_id': Fuel_id | None = None       # ID of domestic hot water fuel
+  'dhw_ef': float = 0.62                     # Energy Factor of DHW System
+  'clothes_drying_fuel_id': Fuel_id | None = None   # ID of clothes drying fuel
+  'cooking_fuel_id': Fuel_id | None = None   # ID of cooking fuel
+  'misc_elec_kwh_per_day': 13.0,      # will be changed in model fitting
+  'misc_elec_seasonality': 0.15,      # will be changed in model fitting
+  'ev_charging_miles_per_day': float = 0.0           # average miles / day of home EV charging
+  'ev_miles_per_kwh': 3.0,            # will be changed in model fitting
+  'ev_seasonality': 0.0,              # will be changed in model fitting
+  'solar_kw': float = 0.0             # kW of home PV solar
+  'solar_kwh_per_kw': 650.0,             # will be changed in model fitting
 '''
   }
 
-  return api_inp
+  return bldg
 
 def calculate_results(inputs):
 
